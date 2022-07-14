a015c8a3117bcb44f064e91246b4923c777598bd
Lugpdev2022 <lugpdev2022@gmail.com>
2022-07-14 17:41:38 -0500

    preguntados

---
+++ src/preguntas.js
@@ -0,0 +1,49 @@
+const preguntas = [
+  {
+    titulo: '¿Cuántos elementos tiene la tabla periódica?',
+    opciones: [
+      { textoRespuesta: '118', isCorrect: true },
+      { textoRespuesta: '92', isCorrect: false },
+      { textoRespuesta: '134', isCorrect: false },
+      { textoRespuesta: '129', isCorrect: false },
+    ],
+  },
+  {
+    titulo: '¿Sobre qué habla el articulo 3 de la constitución?',
+    opciones: [
+      { textoRespuesta: 'credo', isCorrect: false },
+      { textoRespuesta: 'educación', isCorrect: true },
+      { textoRespuesta: 'libertad de expresión', isCorrect: false },
+      { textoRespuesta: 'ninguno de los anteriores', isCorrect: false },
+    ],
+  },
+  {
+    titulo: '¿En qué año se inició la Segunda Guerra Mundial?',
+    opciones: [
+      { textoRespuesta: '1939', isCorrect: true },
+      { textoRespuesta: '1917', isCorrect: false },
+      { textoRespuesta: '1942', isCorrect: false },
+      { textoRespuesta: '1951', isCorrect: false },
+    ],
+  },
+  {
+    titulo: '¿Cómo se llama el resultado de una multiplicación?',
+    opciones: [
+      { textoRespuesta: 'cociente', isCorrect: false },
+      { textoRespuesta: 'residuo', isCorrect: false },
+      { textoRespuesta: 'exponente', isCorrect: false },
+      { textoRespuesta: 'producto', isCorrect: true },
+    ],
+  },
+  {
+    titulo: '¿Cuál es la unidad de medida de la energía?',
+    opciones: [
+      { textoRespuesta: 'gigabyte', isCorrect: false },
+      { textoRespuesta: 'joule', isCorrect: true },
+      { textoRespuesta: 'watt', isCorrect: false },
+      { textoRespuesta: 'amperio', isCorrect: false },
+    ],
+  },
+];
+
+export default preguntas;
